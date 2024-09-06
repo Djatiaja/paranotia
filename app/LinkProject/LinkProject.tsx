d56094f0c7a99337e8a4dec8ad5c579a3f0704a0
djatiaja <djamgt23@gmail.com>
2024-09-06 07:42:37 +0700

    first commit

---
+++ app/LinkProject/LinkProject.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+interface LinkProjectProps {
+    text: string;
+    link: string;
+    target: string;
+}
+export default function LinkProject(props: LinkProjectProps) {
+    return (
+      <Link href={props.link} className="pb-7" target={props.target}>
+        <strong className="font-bold text-2xl  hover:bg-black hover:text-white ease-in-out duration-200">
+            {props.text}
+        </strong>
+      </Link>
+    );
+}
---
+++ app/navbar/Nav.tsx
@@ -0,0 +1,6 @@
+import Link from "next/link";
+import LinkProject from "../LinkProject/LinkProject";
+
+export default function Nav() {
+    return <LinkProject text="feasypol.live" link="/" target="_self" />;
+}
