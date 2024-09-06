51d9b5f54b9336d710628f6686895b49bd370fca
djatiaja <djamgt23@gmail.com>
2024-09-06 19:20:14 +0700

    fix footer

--- app/component/footer/Footer.tsx
+++ app/component/footer/Footer.tsx
@@ -2,7 +2,7 @@ import LinkProject from "../LinkProject/LinkProject";
 
 export default function Footer() {
     return (
-      <div className="fixed mx-auto self-center top-[88vh]">
+      <div className="mx-auto self-center ">
         <div className="flex w-full justify-center gap-12 mt-10">
           <LinkProject text="blog" link="/blogs" target="_self" />
           <LinkProject text="gallery" link="/" target="_self" />
